3e1fd32d75e22715814168f8ff478d3ec3f184f4
lthn99 <lenhung14101999@gmail.com>
2023-04-21 15:13:19 +0700

    [Thực hành] Ứng dụng máy tính đơn giản

---
+++ AutomatedTesting_TDD/src/main/java/ung_dung_mt_don_gian/SimpleCalculator.java
@@ -0,0 +1,13 @@
+package ung_dung_mt_don_gian;
+
+public class SimpleCalculator {
+    public static int add(int first, int second){
+        return first + second;
+        //throw new UnsupportedOperationException();
+    }
+
+    public static int sub(int first, int second){
+        return first - second;
+        //throw new UnsupportedOperationException();
+    }
+}
