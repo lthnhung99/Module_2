64bf0ece47ef27a38cf84bb1fea91b00175591b6
lthn99 <lenhung14101999@gmail.com>
2023-04-21 15:24:17 +0700

    [Thực hành] Tìm giá trị tuyệt đối

---
+++ AutomatedTesting_TDD/src/main/java/gia_tri_tuyet_doi/AbsoluteNumberCalculator.java
@@ -0,0 +1,9 @@
+package gia_tri_tuyet_doi;
+
+public class AbsoluteNumberCalculator {
+    public static int findAbsolute(int number){
+        if(number < 0)
+            return  -number;
+        return number;
+    }
+}
